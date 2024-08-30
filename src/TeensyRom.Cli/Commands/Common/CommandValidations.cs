2d819ae0c4553d8bd720dfffb45a46384898c2f5
metalhexx <metalhexx@outlook.com>
2024-08-30 02:19:45 -0400

    chore: added reusable validations

---
+++ src/TeensyRom.Cli/Commands/Common/CommandValidations.cs
@@ -0,0 +1,47 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeensyRom.Cli.Commands.Common
+{
+    internal static class CommandValidations
+    {
+        //make a validation from this:
+        //!StorageDevice.Equals(string.Empty) && !StorageDevice.IsValueValid(["sd", "usb"])
+
+        public static bool IsValueValid(this string value, string[] validValues) => validValues.Contains(value);
+        public static bool IsValidStorageDevice(this string value) => value.IsValueValid(new[] { "sd", "usb" });
+        public static bool IsValidUnixPath(this string value) => value.StartsWith("/") && value.EndsWith("/");
+        public static bool IsValidFilter(this string value) => value.IsValueValid(new[] { "all", "music", "games", "images" });
+
+        public static ValidationResult ValidateStorageDevice(this string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value) && !value.IsValidStorageDevice())
+            {
+                return ValidationResult.Error("Storage device must be 'sd' or 'usb'.");
+            }
+            return ValidationResult.Success();
+        }
+
+        public static ValidationResult ValidateUnixPath(this string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value) && !value.IsValidUnixPath())
+            {
+                return ValidationResult.Error("Must be a valid unix path.");
+            }
+            return ValidationResult.Success();
+        }
+
+        public static ValidationResult ValidateFilter(this string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value) && !value.IsValidFilter())
+            {
+                return ValidationResult.Error("Filter must be 'all', 'music', 'games', or 'images'.");
+            }
+            return ValidationResult.Success();
+        }
+    }
+}
