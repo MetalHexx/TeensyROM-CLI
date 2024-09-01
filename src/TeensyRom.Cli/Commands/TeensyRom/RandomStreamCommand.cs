fd4a18e0851b2df394e0b72227dce30abc33dabe
metalhexx <metalhexx@outlook.com>
2024-09-01 02:16:14 -0400

    chore updates to random steam docs

--- src/TeensyRom.Cli/Commands/TeensyRom/RandomStreamCommand.cs
+++ src/TeensyRom.Cli/Commands/TeensyRom/RandomStreamCommand.cs
@@ -15,28 +15,31 @@ namespace TeensyRom.Cli.Commands.TeensyRom
         {
             player.StopStream();
 
-            RadHelper.WriteMenu("Random Stream", "Randomly streams files from the specified directory. Includes subdirectories.",
+            RadHelper.WriteMenu("Random Stream", "Randomly streams files from the specified directory and it's subdirectories.",
             [
                "SIDs will stream continuously on play length.",
                "Games, images and demos can be streamed with a timer.",
-               "Game timers will not alter SID play time",
+               "Options selected for timers will persist across commands (will move to settings)",
                "Cache files to fatten your stream. ;)"
             ]);
             var storageType = CommandHelper.PromptForStorageType(settings.StorageDevice);
             settings.Directory = CommandHelper.PromptForDirectoryPath(settings.Directory, "/");
             var filterType = CommandHelper.PromptForFilterType(settings.Filter);
 
-            if (filterType is TeensyFilterType.All or TeensyFilterType.Games)
+            if (filterType is TeensyFilterType.All or TeensyFilterType.Games or TeensyFilterType.Images)
             {   
                 player.SetStreamTime(CommandHelper.PromptGameTimer());
 
-                var overrideSidTIme = PromptHelper.ChoicePrompt("Override SID Time", ["No", "Yes"]) switch
+                if (filterType is TeensyFilterType.All)
                 {
-                    "No" => false,
-                    "Yes" => true,                    
-                    _ => false
-                };
-                player.OverrideSidTIme(overrideSidTIme);
+                    var overrideSidTIme = PromptHelper.ChoicePrompt("Override SID Time", ["No", "Yes"]) switch
+                    {
+                        "No" => false,
+                        "Yes" => true,
+                        _ => false
+                    };
+                    player.OverrideSidTIme(overrideSidTIme);
+                }
             }
 
             if (!settings.ValidateSettings()) return -1;
