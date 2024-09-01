b00eb701adae78ff53bc332b64ede2d273176e93
metalhexx <metalhexx@outlook.com>
2024-08-31 23:57:50 -0400

    fix: added reset to cache command to avoid potential issues

--- src/TeensyRom.Cli/Commands/TeensyRom/CacheCommand.cs
+++ src/TeensyRom.Cli/Commands/TeensyRom/CacheCommand.cs
@@ -1,17 +1,19 @@
-﻿using Spectre.Console;
+﻿using MediatR;
+using Spectre.Console;
 using Spectre.Console.Cli;
 using System.Numerics;
 using System.Reactive.Linq;
 using TeensyRom.Cli.Commands.Common;
 using TeensyRom.Cli.Commands.TeensyRom.Services;
 using TeensyRom.Cli.Helpers;
+using TeensyRom.Core.Commands;
 using TeensyRom.Core.Serial.State;
 using TeensyRom.Core.Storage.Entities;
 using TeensyRom.Core.Storage.Services;
 
 namespace TeensyRom.Cli.Commands.TeensyRom
 {
-    internal class CacheCommand(ISerialStateContext serial, ICachedStorageService storage, IPlayerService player) : AsyncCommand<CacheCommandSettings>
+    internal class CacheCommand(ISerialStateContext serial, ICachedStorageService storage, IPlayerService player, IMediator mediator) : AsyncCommand<CacheCommandSettings>
     {
         public override async Task<int> ExecuteAsync(CommandContext context, CacheCommandSettings settings)
         {
@@ -25,9 +27,14 @@ namespace TeensyRom.Cli.Commands.TeensyRom
             ]);
 
             var storageType = CommandHelper.PromptForStorageType(settings.StorageDevice);
-            settings.Path = CommandHelper.PromptForDirectoryPath(settings.Path, "/test-cache");
+            settings.Path = CommandHelper.PromptForDirectoryPath(settings.Path, "/");
 
             if (!settings.ValidateSettings()) return -1;
+
+            RadHelper.WriteTitle("Resetting TR before caching.  Don't mess with your C64 until caching completed.");
+            AnsiConsole.WriteLine();
+
+            await mediator.Send(new ResetCommand());
 
             RadHelper.WriteLine($"Caching files for {storageType}...");
             AnsiConsole.WriteLine();
